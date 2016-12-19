b97efb2c63f860a7426935fe05b1631af67475ed
Nicholai Main <nmain@measinc.com>
2016-12-19 09:47:54 -0500

    Add library project

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static string GetLibraryVersion()
+        {
+            return typeof(Class1)
+                .Assembly
+                .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+                .InformationalVersion;
+        }
+    }
+}
